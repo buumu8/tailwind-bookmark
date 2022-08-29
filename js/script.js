df0cbea03fb855017bb7b6c66b1fc99267e05208
buumu8 <buumu8@gmail.com>
2022-08-29 18:55:56 +0700

    finalize and publish with netlify

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const tabs = document.querySelectorAll(".tab");
 const panels = document.querySelectorAll(".panel");
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+const logo = document.getElementById("logo");
 
 const onTabClick = (e) => {
   tabs.forEach((tab) => {
@@ -20,5 +23,33 @@ const onTabClick = (e) => {
   //     .classList.remove("hidden");
 };
 
+// Toggle Mobile Menu
+const navToggle = () => {
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+  if (menu.classList.contains("flex")) {
+    logo.setAttribute("src", "./images/logo-bookmark-footer.svg");
+  } else {
+    logo.setAttribute("src", "./images/logo-bookmark.svg");
+  }
+};
+
+// Check if Click Outside
+const clickOutside = (event) => {
+  const el = event.target;
+  if (menu.contains(el) || btn.contains(el)) {
+    if (el.hasAttribute("href")) return navToggle();
+    return;
+  }
+  return btn.classList.contains("open") && navToggle();
+};
+
 // Tabs menu event listener
 tabs.forEach((tab) => tab.addEventListener("click", onTabClick));
+
+// hamburger button listener
+btn.addEventListener("click", navToggle);
+
+// click outside
+window.addEventListener("click", clickOutside);
